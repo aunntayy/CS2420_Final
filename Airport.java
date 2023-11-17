4f5f90a64861eac0823b90688b5b05e5709fbdbc
aunntayy <109895479+aunntayy@users.noreply.github.com>
2023-11-17 14:58:07 -0700

    Add files via upload

---
+++ Airport.java
@@ -0,0 +1,59 @@
+package flightRoutePlanner;
+
+public class Airport {
+
+	String code;
+	String name;
+
+	public Airport(String code, String name) {
+
+		this.code = code;
+		this.name = name;
+	}
+
+	/**
+	 * Get code for airport code
+	 * 
+	 * @return String
+	 * 
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * Set code for airport
+	 * 
+	 * @param String 
+	 * 
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * Get name of the airport
+	 * 
+	 * @return String
+	 * 
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Set name for airport 
+	 * 
+	 * @param String name
+	 * 
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Airport [code=" + code + ", name=" + name + "]";
+	}
+
+}
