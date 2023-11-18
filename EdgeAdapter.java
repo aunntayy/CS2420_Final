e853563a90b791d32a851948d5db601ebcbb8c40
Jesse Cherry <98063092+jessecherry@users.noreply.github.com>
2023-11-17 18:33:26 -0700

    Update and rename DirectedEdgeAdapter.java to EdgeAdapter.java
    Reflects change from using a directed graph to an undirected graph

---
+++ EdgeAdapter.java
@@ -0,0 +1,26 @@
+package flightRoutePlanner;
+
+import edu.princeton.cs.algs4.Edge;
+
+public class EdgeAdapter extends Edge {
+	private final double flightTime;	// weight2
+	
+	public EdgeAdapter(int v, int w, double cost, double flightTime) {
+		super(v, w, cost);
+		this.flightTime = flightTime;
+	}
+	
+	public double getFlightTime() {
+		return flightTime;
+	}
+	
+	@Override
+	public int either() {
+		return super.either();
+	}
+	
+	@Override
+	public int other(int vertex) {
+		return super.other(vertex);
+	}
+}
