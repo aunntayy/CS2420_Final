132891cce098bfddcaeae5ccada8fc4fa94c925a
Jesse Cherry <98063092+jessecherry@users.noreply.github.com>
2023-11-17 18:41:45 -0700

    Update FlightGraph.java
    New constructor with parameter of type In

--- FlightGraph.java
+++ FlightGraph.java
@@ -1,12 +1,17 @@
 package flightRoutePlanner;
 
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.In;
 
 public class FlightGraph {
 	private final EdgeWeightedGraph graph;
 	
 	public FlightGraph(int v) {
 		graph = new EdgeWeightedGraph(v);
+	}
+	
+	public FlightGraph(In in) {
+		graph = new EdgeWeightedGraph(in);
 	}
 	
 	public void addEdge(EdgeAdapter edge) {
