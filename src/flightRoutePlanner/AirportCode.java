d0617c9990923f4c5c502c0c00d9a8462de2e31e
aunntayy <109895479+aunntayy@users.noreply.github.com>
2023-11-21 13:13:11 -0700

    Update AirportCode.java

--- src/flightRoutePlanner/AirportCode.java
+++ src/flightRoutePlanner/AirportCode.java
@@ -1,5 +1,10 @@
 package flightRoutePlanner;
-
+/**
+ * 
+ * Enum class for airport code.
+ * 
+ * @authors Chanphone Visathip and Jesse Cherry
+ */
 public enum AirportCode {
 	SEA, SFO, LAX, LAS, PHX, SLC, DEN, DFW, IAH, MSP, ORD, DTW, ATL, JFK, MIA
 }
