2eed09a47a4c85d195f53d94bd80b3e295350741
aunntayy <109895479+aunntayy@users.noreply.github.com>
2023-11-21 13:15:19 -0700

    Update EdgeAdapter.java

--- src/flightRoutePlanner/EdgeAdapter.java
+++ src/flightRoutePlanner/EdgeAdapter.java
@@ -1,7 +1,15 @@
 package flightRoutePlanner;
 
 import edu.princeton.cs.algs4.Edge;
+/**
+ * This class help generate weigh for graph
+ *
+ *@authors Chanphone Visathip and Jesse Cherry
+ *
+ */
 
+
+	
 public class EdgeAdapter extends Edge {
 	private final double flightTime;	// weight2
 	
