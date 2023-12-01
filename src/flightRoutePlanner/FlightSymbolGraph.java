54ebe155cd3ff84d7f45e7f8a91662db679474f0
Jesse Cherry <98063092+jessecherry@users.noreply.github.com>
2023-11-30 20:47:39 -0700

    Update FlightSymbolGraph.java
    Altered constructor to accept additional parameter to indicate desired weight (0 for cost or 1 for time), chooses which weight to add to graph based on that. Now custom class DualWeightedGraph is no longer necessary and EdgeWeightedGraph from Algs4 is sufficient.

--- src/flightRoutePlanner/FlightSymbolGraph.java
+++ src/flightRoutePlanner/FlightSymbolGraph.java
@@ -1,6 +1,8 @@
 package flightRoutePlanner;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.EdgeWeightedGraph;
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.ST;
 
@@ -23,26 +25,30 @@ import edu.princeton.cs.algs4.ST;
  *
  *	Based on code by Robert Sedgewick and Kevin Wayne.	
  *
- *	@author Jesse Cherry 
+ *	@author Jesse Cherry
  *  @author Chanphone Visathip
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
  */
 public class FlightSymbolGraph {
     private ST<String, Integer> st;  	// string -> index
     private String[] keys;           	// index  -> string
-    private DualWeightedGraph graph;	// the underlying graph
+    private EdgeWeightedGraph graph;	// the underlying graph
     
-    // TODO implement edge-weighting for flight cost and duration
-
+    
     /**
      * Initializes a graph from a file using the specified delimiter.
-     * Each line in the file contains
-     * the name of a vertex, followed by a list of the names
-     * of the vertices adjacent to that vertex, separated by the delimiter.
+     * Each line in the file contains the name of a vertex, followed an adjacent
+     * vertex and two weights (cost and time respectively), separated by the delimiter.
      * @param filename the name of the file
      * @param delimiter the delimiter between fields
+     * @param weightType the type of weight (0 for cost, 1 for flight time)
      */
-    public FlightSymbolGraph(String filename, String delimiter) {
-        st = new ST<String, Integer>();
+    public FlightSymbolGraph(String filename, String delimiter, int weightType) {
+        if (weightType != 0 && weightType != 1)
+        	throw new IllegalArgumentException("weightType must be 0 for cost or 1 for time");
+        
+    	st = new ST<String, Integer>();
 
         // First pass builds the index by reading strings to associate
         // distinct strings with an index
@@ -64,20 +70,28 @@ public class FlightSymbolGraph {
 
         // second pass builds the graph by connecting first vertex on each
         // line to all others
-        graph = new DualWeightedGraph(st.size());
+        graph = new EdgeWeightedGraph(st.size());
         in = new In(filename);
         while (in.hasNextLine()) {
             String[] a = in.readLine().split(delimiter);
             int v = st.get(a[0]);
             for (int i = 1; i + 2 < a.length; i+=3) {
                 int w = st.get(a[i]);
-                double cost = Double.parseDouble(a[i + 1]);
-                double flightTime = Double.parseDouble(a[i + 2]);
-                graph.addEdge(v, w, cost, flightTime);
+                int cost = Integer.parseInt(a[i + 1]);
+                int flightTime = Integer.parseInt(a[i + 2]);
+                // if cost
+                if (weightType == 0) {
+                    graph.addEdge(new Edge(v, w, cost));
+                }
+                // if time
+                else {
+                	graph.addEdge(new Edge(v, w, flightTime));
+                }
             }
         }
     }
 
+    
     /**
      * Does the graph contain the vertex named {@code s}?
      * @param s the name of a vertex
@@ -86,6 +100,7 @@ public class FlightSymbolGraph {
     public boolean contains(String s) {
         return st.contains(s);
     }
+    
 
     /**
      * Returns the integer associated with the vertex named {@code s}.
@@ -108,21 +123,14 @@ public class FlightSymbolGraph {
         return keys[v];
     }
 
+    
     /**
      * Returns the graph associated with the symbol graph. It is the client's responsibility
      * not to mutate the graph.
      * @return the graph associated with the symbol graph
      */
-    public DualWeightedGraph graph() {
+    public EdgeWeightedGraph graph() {
         return graph;
-    }
-
-    public double getCost(int v, int w) {
-    	return graph.getCost(v, w);
-    }
-    
-    public double getFlightTime(int v, int w) {
-    	return graph.getFlightTime(v, w);
     }
     
 
