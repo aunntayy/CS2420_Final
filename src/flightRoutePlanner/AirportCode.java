aab0d10afadec72511fb7b32ae096a791922257d
aunntayy <109895479+aunntayy@users.noreply.github.com>
2023-12-01 18:30:24 -0700

    Update AirportCode.java

--- src/flightRoutePlanner/AirportCode.java
+++ src/flightRoutePlanner/AirportCode.java
@@ -1,4 +1,5 @@
 package flightRoutePlanner;
+
 /**
  * 
  * Enum class for airport code.
@@ -6,5 +7,27 @@ package flightRoutePlanner;
  * @authors Chanphone Visathip and Jesse Cherry
  */
 public enum AirportCode {
-	SEA, SFO, LAX, LAS, PHX, SLC, DEN, DFW, IAH, MSP, ORD, DTW, ATL, JFK, MIA
+	SEA("Seattle-Tacoma International Airport"), SFO("San Francisco International Airport"),
+	LAX("Los Angeles International Airport"), LAS("Harry Reid International Airport"),
+	PHX("Phoenix Sky Harbor International Airport"), SLC("Salt Lake City International Airport"),
+	DEN("Denver International Airport"), DFW("Dallas/Fort Worth International Airport"),
+	IAH("George Bush Intercontinental Airport"), MSP("Minneapolis St Paul International Airport"),
+	ORD("O'Hare International Airport"), DTW("Detroit Metropolitan Airport"),
+	ATL("Hartsfield Jackson Atlanta International Airport"), JFK("John F. Kennedy International Airport"),
+	MIA("Miami International Airport");
+
+	private String name;
+
+
+	AirportCode(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public String toString() {
+	    return name;
+	}
 }
